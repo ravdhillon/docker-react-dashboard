5ad7126798db7f799c617abc5540dba076099178
ravdhillon <ravisher.dhillon@gmail.com>
2020-11-29 08:01:28 -0500

    Added the test.

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import { iteratee } from 'lodash';
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './App'
+
+it('renders without crashing', () => {
+    const div = document.createElement('div')
+    // ReactDOM.render(<App />, div)
+    // ReactDOM.unmountComponentAtNode(div);
+});
